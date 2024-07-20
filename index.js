ba4a35a4bf8f88bffd071c481cc56249c6001f93
Rifan Muttaqin <rifanmuttaqin@gmail.com>
2024-07-20 12:52:52 +0700

    -- sendMessageByNumber function

--- index.js
+++ index.js
@@ -4,41 +4,62 @@ const makeWASocket = require("@whiskeysockets/baileys").default
 let sock;
 
 async function connectionLogic() {
-    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
+    const {state, saveCreds} = await useMultiFileAuthState('auth_info_baileys')
     sock = makeWASocket({
         // can provide additional config here
         printQRInTerminal: true,
-        auth:state,
+        auth: state,
     })
 
-    sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect, qr } = update || {}
+    sock.ev.on('connection.update', async (update) => {
+        const {connection, lastDisconnect, qr} = update || {}
 
-        if(connection === 'close'){
+        if (connection === 'close') {
             const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
-            if(shouldReconnect){
+            if (shouldReconnect) {
                 connectionLogic();
             }
-        } else if(connection === 'open'){
+        } else if (connection === 'open') {
             console.log('Connection opened');
         }
     })
 
     // reply message
-    async function sendMessage(from) {
+    async function sendMessage(from, text) {
         try {
-            const sentMsg = await sock.sendMessage(from, { text: "Siap bos ada yang bisa dibantu ?" });
+            const sentMsg = await sock.sendMessage(from, {text: text});
             console.log("Message sent successfully", sentMsg);
-        } catch (err){
+        } catch (err) {
             console.error("Failed to send message", err);
+        }
+    }
+
+    async function sendMessageByNumber(number, textString){
+        try {
+            const checkNumbers = await checkNumber(number);
+            if(checkNumbers.exists){
+                const sentMsg = await sendMessage(checkNumbers.jid, {text: textString});
+                return sentMsg;
+            }
+        } catch (err) {
+            console.error("Failed to send message", err);
+        }
+    }
+
+    async function checkNumber(param) {
+        const id = param
+        const [result] = await sock.onWhatsApp(id)
+        if (result) {
+            return result; // sample result = { exists: true, jid: '6285735727063@s.whatsapp.net' }
         }
     }
 
     async function keyTrigger(msgText, from) {
         if (msgText) {
-            if( msgText.toLowerCase() === "halo"){
+            if (msgText.toLowerCase().startsWith('!mulai')) {
                 try {
-                    await sendMessage(from);
+                    // other logic for bot convertation
+                    await sendMessage(from,'Baik sistem dimulai, berikut daftar untuk memberi saya perintah : ');
                 } catch (error) {
                     console.error("Failed to send reply", error);
                 }
@@ -46,10 +67,9 @@ async function connectionLogic() {
         }
     }
 
-    sock.ev.on("messages.upsert", async ({ messages }) => {
-        // console.log("ori", messages);
+    sock.ev.on("messages.upsert", async ({messages}) => {
         for (const message of messages) {
-            if (message.message && message.message.conversation) {
+            if (message.key.remoteJid && message.message.conversation) {
                 const msgText = message.message.conversation;
                 const from = message.key.remoteJid;
                 await keyTrigger(msgText, from)
@@ -57,8 +77,7 @@ async function connectionLogic() {
         }
     });
 
-    sock.ev.on ('creds.update', saveCreds)
-
+    sock.ev.on('creds.update', saveCreds)
 }
 
 connectionLogic();
